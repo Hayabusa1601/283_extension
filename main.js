6be0eeddb5f2d203313d8cd296c057483f23f21c
Hayabusa1601 <1601hayabusa@gmail.com>
2022-03-21 16:55:50 +0900

    [Add] 別ウィンドウで起動

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-void function open_shiny_colors() {
-    window.open('https://shinycolors.enza.fun/home')
+function open_shiny_colors() {
+  window.open("https://shinycolors.enza.fun/", null, 'width=1136,height=640');
 }
 
-var btn = document.getElementById('start_up');
-
-btn.addEventListener('click', open_shiny_colors, false)
+document.getElementById("start_up").onclick = function() {
+    open_shiny_colors();
+};
 

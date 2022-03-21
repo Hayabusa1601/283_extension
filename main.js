adab15b69ca27c2703c9f42a96b1157f65b6781d
Hayabusa1601 <1601hayabusa@gmail.com>
2022-03-21 16:39:33 +0900

    [Add] 名前の変更

---
+++ main.js
@@ -0,0 +1,8 @@
+void function open_shiny_colors() {
+    window.open('https://shinycolors.enza.fun/home')
+}
+
+var btn = document.getElementById('start_up');
+
+btn.addEventListener('click', open_shiny_colors, false)
+

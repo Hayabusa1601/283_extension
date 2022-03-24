0ffd1f486676bfae8056c1715289f1bc78e2deb0
Hayabusa1601 <1601hayabusa@gmail.com>
2022-03-24 15:54:04 +0900

    [Add] ウィンドウサイズを三種類追加

--- js/main.js
+++ js/main.js
@@ -1,8 +1,53 @@
-function open_shiny_colors() {
-  window.open("https://shinycolors.enza.fun/", null, 'width=1136,height=640');
+function get_window_size() {
+  let elements = document.getElementsByName('answer');
+  let len = elements.length;
+  let checkValue = '';
+
+  for(let i = 0; i < len; i++) {
+    if(elements.item(i).checked){
+      checkValue = elements.item(i).value;
+    }
+  }//answerグループのうちどれがtrueであるかを確認
+
+  //alert(checkValue);
+  return checkValue;//チェックが入ってる値を返す
+}
+
+function open_shiny_colors( windowSize ) {
+  let width, height;
+
+  switch (windowSize) {
+    case 'original':
+      width = 1148;
+      height = 680;
+      break;
+    case 'big':
+      width = 1722;
+      height = 1020;
+      break;
+    case 'small': 
+      width = 570;
+      height = 364;
+      break;
+    default:
+      alert('error!')
+      width = 1148;
+      height = 680;
+      break;
+  };//windowSizeの値に応じて縦横比を変更
+
+  /*起動*/ 
+  var widowFeatures = 'width=' + width + ',height=' + height;
+  alert(widowFeatures);
+  window.open("https://shinycolors.enza.fun/", 'shinycolors', widowFeatures);
+
+
 }
 
 document.getElementById("start_up").onclick = function() {
-    open_shiny_colors();
+  var windowSize = get_window_size();// ウィンドウサイズの種類を取得
+  //alert(windowSize);
+
+  open_shiny_colors(windowSize);//シャニマス起動
 };
 
